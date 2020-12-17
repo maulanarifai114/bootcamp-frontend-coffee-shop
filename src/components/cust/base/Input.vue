ba44d1c629b0d4fd5f35a52e4b912e4261c5557c
maulanarifai114 <maulanarifai114@gmail.com>
2020-12-17 10:50:02 +0700

    make delivery and time for product detail

---
+++ src/components/cust/base/Input.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="d-flex flex-column">
+    <label :for="id" class="label">{{label}}</label>
+    <input :type="type" :id="id" :placeholder="placeholder" class="placeholder input" required>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputAuth',
+  props: ['id', 'type', 'placeholder', 'label']
+}
+</script>
+
+<style lang="scss" scoped>
+
+.input {
+  width: 256px;
+  height: 47px;
+  border: 1px solid rgba(186, 186, 186, 0.35);
+  border-radius: 10px;
+  padding: 0 14px;
+}
+
+.input:focus {
+  outline: none;
+}
+
+.placeholder::placeholder {
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 22px;
+  color: #9F9F9F;
+}
+
+.label {
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+  color: #4F5665;
+}
+
+</style>
--- src/components/cust/module/DeliveTime.vue
+++ src/components/cust/module/DeliveTime.vue
@@ -3,24 +3,50 @@
     <div class="del-time">
       <h4>Delivery and Time</h4>
       <div class="d-flex mb-5">
-        <ButtonDelive label="Dine In"></ButtonDelive>
+        <div class="btn-group-toggle" data-toggle="buttons">
+          <label class="btn mr-3" for="dine" :class="deliver.includes('dine')? 'selected':'unselected'">
+            <input type="radio" name="deliver" id="dine" v-model="deliver" value="dine"> Dine In
+          </label>
+          <label class="btn mr-3" for="door" :class="deliver.includes('door')? 'selected':'unselected'">
+            <input type="radio" name="deliver" id="door" v-model="deliver" value="door"> Door Delivery
+          </label>
+          <label class="btn mr-3" for="pick" :class="deliver.includes('pick')? 'selected':'unselected'">
+            <input type="radio" name="deliver" id="pick" v-model="deliver" value="pick"> Pick Up
+          </label>
+        </div>
       </div>
       <div class="d-flex align-items-center mb-5">
         <div class="label-tag mr-now">Now</div>
+        <div class="btn-group-toggle" data-toggle="buttons">
+          <label class="btn mr-3" for="yes" :class="now.includes('yes')? 'selected':'unselected'">
+            <input type="radio" name="now" id="yes" v-model="now" value="yes"> Yes
+          </label>
+          <label class="btn mr-3" for="no" :class="now.includes('no')? 'selected':'unselected'">
+            <input type="radio" name="now" id="no" v-model="now" value="no"> No
+          </label>
+        </div>
       </div>
       <div class="d-flex align-items-center mb-5">
         <div class="label-tag mr-set">Set time</div>
+        <Input id="time" type="text" placeholder="Enter time for reservation"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ButtonDelive from '../base/ButtonDelive'
+import Input from '../base/Input'
+
 export default {
   name: 'DeliveTime',
   components: {
-    ButtonDelive
+    Input
+  },
+  data () {
+    return {
+      deliver: [],
+      now: []
+    }
   }
 }
 </script>
@@ -79,6 +105,12 @@ h4 {
   letter-spacing: 0em;
   text-align: left;
   box-shadow: 0px 6px 20px 0px rgba(137, 85, 55, 0.4);
+  transition: .3s;
+}
+
+.selected:hover {
+  color: white!important;
+  opacity: .8;
 }
 
 .unselected {
@@ -94,6 +126,12 @@ h4 {
   line-height: 30px;
   letter-spacing: 0em;
   text-align: left;
-  border: 1px solid rgba(186, 186, 186, 0.35)
+  border: 1px solid rgba(186, 186, 186, 0.35);
+  transition: .3s;
+}
+
+.unselected:hover {
+  color: #9F9F9F!important;
+  opacity: .8;
 }
 </style>
